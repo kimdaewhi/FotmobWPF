1c5c8baa65f41e09ab1c9e62df8df4f0bca50f0a
김대휘 <kimdaewhi@naver.com>
2023-12-26 17:20:50 +0900

    선수 포지션 정보 렌더 기능 추가(완성)

--- Main/Views/PlayerInfoPage.xaml.cs
+++ Main/Views/PlayerInfoPage.xaml.cs
@@ -59,7 +59,99 @@ namespace Main.Views
             viewModels.SelectedPlayerNation = await NationController.GetNationDetail(viewModels.SelectedPlayerDetail.NationID);
             viewModels.SelectedPlayerTeam = await TeamController.GetTeamDetail(viewModels.SelectedPlayerDetail.ClubID);
 
+            string mainPosition = viewModels.SelectedPlayerDetail.Position.Split(',')[0].Trim();
+            RenderPlayerPosition(mainPosition);
+
             this.DataContext = viewModels;
+        }
+
+
+
+        // 선수 포지션 필드 렌더
+        private void RenderPlayerPosition(string mainPosition)
+        {
+            Dictionary<string, Border> borderControls = new Dictionary<string, Border>
+            {
+                // Att
+                { "ST", txtPos_ST },
+                { "LW", txtPos_LW },
+                { "AM", txtPos_AM },
+                { "RW", txtPos_RW },
+
+                // Mid
+                { "LM", txtPos_LM },
+                { "CM", txtPos_CM },
+                { "RM", txtPos_RM },
+                { "DM", txtPos_DM },
+
+                // Def
+                { "LWB", txtPos_LWB },
+                { "RWB", txtPos_RWB },
+                { "LB", txtPos_LB },
+                { "RB", txtPos_RB },
+                { "CB", txtPos_CB },
+
+                // GK
+                { "GK", txtPos_GK },
+            };
+
+            Dictionary<string, string> positionKor = new Dictionary<string, string>
+            {
+                // Att
+                { "ST", "스트라이커" },
+                { "LW", "왼쪽 윙어"},
+                { "AM", "중앙 공격형 미드필더" },
+                { "RW", "오른쪽 윙어" },
+
+                // Mid
+                { "LM", "왼쪽 미드필더" },
+                { "CM", "중앙 미드필더" },
+                { "RM", "오른쪽 미드필더" },
+                { "DM", "중앙 수비형 미드필더" },
+
+                // Def
+                { "LWB", "왼쪽 윙백" },
+                { "RWB", "오른쪽 윙백" },
+                { "LB", "왼쪽 수비수" },
+                { "RB", "오른쪽 수비수" },
+                { "CB", "중앙 수비수" },
+
+                // GK
+                { "GK", "골키퍼" },
+            };
+
+            // Main 포지션 렌더
+            borderControls[mainPosition].Visibility = Visibility.Visible;
+            Color color = (Color)ColorConverter.ConvertFromString(viewModels.SelectedPlayerTeam.ColorCode);
+            borderControls[mainPosition].Background = new SolidColorBrush(color);
+
+            txt_basePosition.Text = positionKor[mainPosition];
+
+
+
+            // Sub 포지션 렌더
+            if (viewModels.SelectedPlayerDetail.Position.Split(',').Length > 1)
+            {
+                for (int i = 1; i < viewModels.SelectedPlayerDetail.Position.Split(',').Length; i++)
+                {
+                    string currSubPos = (viewModels.SelectedPlayerDetail.Position.Split(',')[i].Trim());
+                    borderControls[currSubPos].Visibility = Visibility.Visible;
+
+                    // Sub 포지션 첫 번째면 투명도 없이, 후순위 Sub 포지션이면 투명도 설정
+                    if(i <= (viewModels.SelectedPlayerDetail.Position.Split(',').Length / 2))
+                    {
+                        borderControls[currSubPos].Background = new SolidColorBrush(Color.FromRgb(160, 160, 160));
+                    }
+                    else
+                    {
+                        borderControls[currSubPos].Background = new SolidColorBrush(Color.FromArgb(90, 160, 160, 160));
+                    }
+                    txt_otherPosition.Text += positionKor[viewModels.SelectedPlayerDetail.Position.Split(',')[i].Trim()] + ", ";
+                }
+            }
+            txt_otherPosition.Text = txt_otherPosition.Text.Substring(0, txt_otherPosition.Text.Length - 2);
+
+
         }
 
 
